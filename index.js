855e1f36ee0d1a93eb83d4778063526499c23195
melipescu <popescumelinda76@gmail.com>
2026-04-19 22:43:50 +0300

    created the random color button

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-// const body = document.getElementsByTagName("body")[0];
+const color = document.getElementsByTagName('body')[0]
 
-// function setColor(name) {
-//     body.style.backgroundColor = name;
-// }
+function colorChange(name) {
+    color.style.backgroundColor = name;
+}
 
-// function randomColor() {
-//     const red = Math.round(Math.random() * 255)
-//     const green = Math.round(Math.random() * 255)
-//     const blue = Math.round(Math.random() * 255)
+function setRandom() {
+    const red = Math.round(Math.random() * 255)
+    const green = Math.round(Math.random() * 255)
+    const blue = Math.round(Math.random() * 255)
 
-//     const color = `rgb(${red}, ${green}, ${blue})`
-//     body.style.backgroundColor = color;
-// }
-
-const body = document.getElementsByTagName("body")[0];
-
-function setColor(name) {
-    body.style.backgroundColor = name;
+    const colorRandom = `rgb(${red}, ${green}, ${blue})`
+    color.style.backgroundColor = colorRandom;
 }
